03b37903589ba09221130eba368dce9959a15c56
bernd-clemenz <bernd.clemenz@gmail.com>
2018-10-20 19:21:46 +0200

    some unit tests.

--- src/test/java/de/isc/cmdq/service/CmdImplTest.java
+++ src/test/java/de/isc/cmdq/service/CmdImplTest.java
@@ -1,11 +1,11 @@
 package de.isc.cmdq.service;
 
 import de.isc.cmdq.conf.ServiceConfig;
+import de.isc.cmdq.domain.CmdRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -24,6 +24,16 @@ public class CmdImplTest {
 
   @Test
   public void test002AddNull() {
+    Assertions.assertThrows(NullPointerException.class,() -> {
+      m_cmd.add(null);
+    });
+  }
 
+  @Test
+  public void test003Add() {
+    CmdRequest req = new CmdRequest();
+    req.setCmdName("name");
+    String id = m_cmd.add(req);
+    Assertions.assertNotNull(id);
   }
 }
